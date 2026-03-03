7eff661252f9d61620144d5dbcb6836efa099dce
QuachNguyenChiHung <bacon3632@gmail.com>
2026-03-03 11:31:30 +0700

    added expo to use tailwind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-[50px] font-bold text-blue-500">Hello NativeWind!</Text>
+    </View>
+  );
+}
